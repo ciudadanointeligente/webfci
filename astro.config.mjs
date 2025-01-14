7c5990ce2f084d0b5bbc6dc99553a4adb7aa226e
Felipe Perry Donoso <felipe.perry@vivaldi.net>
2025-01-14 18:36:17 -0300

    prueba imgs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [tailwind(), alpinejs()],
+});
+
